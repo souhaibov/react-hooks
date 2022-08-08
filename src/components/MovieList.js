9712ffff11ea1cc0bab30ea09ec54dd319475a04
YOUR NAMEsouhaibov <YOUR EMAILsouhaibov6@gmail.com>
2022-08-08 10:36:04 +0100

    correction filter

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,28 +1,24 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React from "react";
+import MovieCard from "./MovieCard";
 
-
-const MovieList = ({text,movies,rate}) => {
-
-  
+const MovieList = ({ text, movies, rate }) => {
   return (
     <div>
-    
       <div>
-      {movies?.filter((el)=>el.rate===rate).map((data,i)=>(
-              <MovieCard key={i} movie={data}/>
-          ))} 
-      {movies?.filter((el)=>el.title.toUpperCase().includes(text.toUpperCase())).map((data,i)=>(
-              <MovieCard key={i} movie={data}/>
-          ))} 
-      
-          
+        {movies
+          ?.filter((el) => (rate === 0 ? el : el.rate >= rate && el ))
+          .filter((el) => el.title.toUpperCase().includes(text.toUpperCase()))
+          .map((data, i) => (
+            <MovieCard key={i} movie={data} />
+          ))}
+        {/* {movies
+          ?.filter((el) => el.title.toUpperCase().includes(text.toUpperCase()))
+          .map((data, i) => (
+            <MovieCard key={i} movie={data} />
+          ))} */}
       </div>
-      
-      
-      
     </div>
-  )
-}
+  );
+};
 
-export default MovieList
+export default MovieList;
